4fc72c205108dba89fdb974e079a0fbfbed52b5b
Hosein <mr.hrezaei@gmail.com>
2026-06-26 07:48:36 +0100

    Add collect_experiences function to BabyAI-Text runner

--- mappo/runner/shared/babyai_text_runner.py
+++ mappo/runner/shared/babyai_text_runner.py
@@ -15,47 +15,17 @@ class BabyAITextRunner(VirtualHomeRunner):
 
         total_num_steps = 0
         for episode in tqdm(range(episodes), desc="episodes"):
-            finished_rewards = []
-            finished_waste = []
-            for step in tqdm(range(self.episode_length), desc="steps"):
-                # Sample actions
-                values, actions, action_tokens, log_probs = self.collect(step)
 
-                # Obser reward and next obs
-                obs, rewards, dones, ava, infos = self.envs.step(actions)
+            collect_logs = self.collect_experiences(episode)
 
-                for i in range(self.n_rollout_threads):
-                    if dones[i] or self.envs.envs.envs[i].steps_remaining == 0:
-                        finished_rewards.append(rewards[i])
-                        info = self.envs.envs.envs[i].metadata.get('info_before_reset', {})
-                        gold_steps = info.get('gold_steps', 0)
-                        actual_steps = info.get('step_count', 0)
-                        finished_waste.append(actual_steps - gold_steps)
-                        global_step = total_num_steps + step * self.n_rollout_threads + i
-                        print(
-                            f"global_step={global_step}, episodic_return={rewards[i]}, episodic_length={0}, waste_frames={actual_steps - gold_steps}")
-                        self.writter.add_scalar("charts/episodic_return", rewards[i], global_step)
-                        self.writter.add_scalar("charts/episodic_length", 0, global_step)
-                        #self.writter.add_scalar("charts/waste_frames", actual_steps - gold_steps, global_step)
-                        #break Original virtualhome runner has break here! I don't know why?!
-
-                # insert data into buffer
-                data = obs, rewards, dones, ava, values, \
-                    actions, action_tokens, log_probs
-                self.insert(data)
-
-            total_num_steps = (episode + 1) * self.episode_length * self.n_rollout_threads
-
-            success_per_episode =  [1 if r > 0 else 0 for r in finished_rewards]
             # compute return and update network
             self.before_update()
             # self.trainer.prep_training()
             train_infos = self.trainer.train(self.buffer)
             self.buffer.after_update()
 
-            train_infos["success_rate"] = sum(success_per_episode) / len(success_per_episode)
-            if finished_waste:
-                train_infos["waste_frames"] = sum(finished_waste)
+            train_infos["success_rate"] = collect_logs["success_rate"]
+            train_infos["waste_frames"] = collect_logs["finished_waste"]
             # save model
             if (episode == episodes - 1):
                 self.save(episode)
@@ -64,3 +34,41 @@ class BabyAITextRunner(VirtualHomeRunner):
             if episode % self.log_interval == 0:
                 print("total_num_steps: ", total_num_steps)
                 self.log_train(train_infos, total_num_steps)
+
+    def collect_experiences(self, episode):
+        finished_rewards = []
+        finished_waste = []
+        total_num_steps = (episode + 1) * self.episode_length * self.n_rollout_threads
+        logs = {}
+        for step in tqdm(range(self.episode_length), desc="steps"):
+            # Sample actions
+            values, actions, action_tokens, log_probs = self.collect(step)
+
+            # Obser reward and next obs
+            obs, rewards, dones, ava, infos = self.envs.step(actions)
+
+            for i in range(self.n_rollout_threads):
+                if dones[i] or self.envs.envs.envs[i].steps_remaining == 0:
+                    finished_rewards.append(rewards[i])
+                    info = self.envs.envs.envs[i].metadata.get('info_before_reset', {})
+                    gold_steps = info.get('gold_steps', 0)
+                    actual_steps = info.get('step_count', 0)
+                    finished_waste.append(actual_steps - gold_steps)
+                    #self.envs.envs.envs[i].max_steps = 40
+                    global_step = total_num_steps + step * self.n_rollout_threads + i
+                    print(
+                        f"global_step={global_step}, episodic_return={rewards[i]}, episodic_length={0}, waste_frames={actual_steps - gold_steps}")
+                    self.writter.add_scalar("charts/episodic_return", rewards[i], global_step)
+                    self.writter.add_scalar("charts/episodic_length", 0, global_step)
+                    # self.writter.add_scalar("charts/waste_frames", actual_steps - gold_steps, global_step)
+                    # break Original virtualhome runner has break here! I don't know why?!
+
+            # insert data into buffer
+            data = obs, rewards, dones, ava, values, \
+                actions, action_tokens, log_probs
+            self.insert(data)
+
+        success_per_episode =  [1 if r > 0 else 0 for r in finished_rewards]
+        logs["success_rate"] = sum(success_per_episode) / len(success_per_episode)
+        logs["finished_waste"] = sum(finished_waste)
+        return logs
